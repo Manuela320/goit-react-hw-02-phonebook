b21faa367518104edab57fc3176200a41efef692
Manuela320 <manuelaandricioaei63@gmail.com>
2024-09-10 11:22:13 +0300

    phonebook

---
+++ src/components/ContactListItem.jsx
@@ -0,0 +1,12 @@
+import Reeact from 'react';
+
+const ContactListItem = ({ contact, onDeleteContact }) => {
+    return (
+        <li>
+            {contact.name}: {contact.number}
+            <button onClick={() => onDeleteContact(contact.id)}>Delete</button>
+        </li>
+    );
+};
+
+export default ContactListItem;
